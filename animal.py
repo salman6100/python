524d7fce21b5aac22a6bbb4cec2f24e75309ff44
salman6100 <smohyudin@gmail.com>
2016-02-02 00:31:24 -0500

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,9 @@
+#printing different bird catenating with message
+
+bird = [ 'eagle','owl', 'crow']
+for list in bird:
+
+print ( list.title() + " They are super great!")
+
+print (list + " lovelable")
+
