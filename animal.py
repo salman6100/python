77499a7ba7dcc051e0f6799fb73678a37973ed00
salman6100 <smohyudin@gmail.com>
2016-02-03 19:57:22 -0500

    Delete animal.py

--- animal.py
+++
@@ -1,9 +0,0 @@
-#printing different bird catenating with message
-
-bird = [ 'eagle','owl', 'crow']
-for list in bird:
-
-print ( list.title() + " They are super great!")
-
-print (list + " lovelable")
-
